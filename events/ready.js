3757645c90c4089d8f1f6a75c4f81c7a1c986be5
Froggi22 <daniel.heinstedt@gmail.com>
2021-08-17 20:37:04 +0200

    v2021.08.17.4
    console.log > Ready

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,8 @@ const chalk = require('chalk')
 
 module.exports = {
 	async run (client) {
-		console.log(`${chalk.bgHex('#00FF00')('    ')} Ready! Logged in as ${client.user.tag}`)
+		const date = new Date().toISOString().replace('T', ' ').substr(0, 19)
+		console.log(`${chalk.bgHex('#00FF00')('    ')} ${date} || Ready! Logged in as ${client.user.tag}`)
 		client.user.setPresence({ activities: [{ type: 'LISTENING', name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 	}
 }
