0686dde4fe3d810cebcb0747e2abe86ce7bdb0a1
Froggi22 <daniel.heinstedt@gmail.com>
2021-08-31 12:14:23 +0200

    2021.08.31
    Created Keys.js
    Basic outline

---
+++ commands/Keys/keys.js
@@ -0,0 +1,31 @@
+module.exports = {
+	description: 'Key Guides',
+	options: [
+		{
+			name: 'pawn-bishop-king',
+			description: 'Keys in Both pawn and bishop + king buildings',
+			type: 'SUB_COMMAND',
+			options: [
+				{
+					name: 'Key',
+					description: 'What kind of key',
+					type: 'STRING',
+					choices: 'a'
+				}
+			]
+		},
+		{
+			name: 'other',
+			description: 'Other locations',
+			type: 'SUB_COMMAND',
+			options: [
+				{
+					name: 'Key',
+					description: 'What kind of key',
+					type: 'STRING',
+					choices: 'a'
+				}
+			]
+		}
+	]
+}
