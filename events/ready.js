543ad0a11f67e486cda12a6ee04a50f74b57513f
Froggi22 <daniel.heinstedt@gmail.com>
2021-09-16 08:36:25 +0200

    2021.09.16.0

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
 		client.guilds.cache.forEach((guild) => {
 			guilds = guilds.concat(guild).concat(' || ')
 		})
-		console.log(`----- Guilds -----\n${guilds}\n----- ${client.guilds.cache.size} Servers -----`)
+		console.log(`----- Guilds -----  ${guilds}  ----- ${client.guilds.cache.size} Servers -----`)
 		const date = new Date().toISOString().replace('T', ' ').substr(0, 19)
 		console.log(`${chalk.bgHex('#00FF00')('    ')} ${date} || Ready! Logged in as ${client.user.tag}`)
 		client.user.setPresence({ activities: [{ type: 'LISTENING', name: `/Help | In ${client.guilds.cache.size} Servers` }] })
