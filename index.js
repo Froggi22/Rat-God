04b991c4ae51a9b1bef27e1fc02e7adbe427abec
Froggi22 <daniel.heinstedt@gmail.com>
2021-11-06 17:17:56 +0100

    Revert "fix GUILD_MEMBERS_TIMEOUT"
    This reverts commit 1ab4d735ee7ed9f1c86de8d9e29192159077e328.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const { readdirSync } = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 client.commands = new Collection();
 client.cooldowns = new Collection();
 client.login(process.env.token);
