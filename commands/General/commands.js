87af66c2e601b0e1d60596c1cd9f3fb1939f7782
Froggi22 <daniel.heinstedt@gmail.com>
2021-12-13 16:29:55 +0100

    2021.12.13.1

--- commands/General/commands.js
+++ commands/General/commands.js
@@ -7,7 +7,7 @@ module.exports = {
 		interaction.reply({
 			embeds: [new MessageEmbed()
 				.setAuthor("A Complete List Of Commands For Rat God", embedDesign.ratGodImage, embedDesign.wikiMain)
-				.setDescription("A list of all functioning commands for Rat God")
+				.setDescription("A list of all functioning commands for Rat God:")
 				.setColor(embedDesign.color)
 				.addFields(
 					{ name: "Ammo", value: "`12x70mm`\n`20x70mm`\n`23x70mm`\n`9x18mm Makarov`\n`7.62x25mm Tokarev`\n`9x19mm Parabellum`\n`.45 ACP`\n`9x21mm Gyurza`\n`5.7x28mm FN`\n`4.6x30mm HK`\n`9x39mm`\n`.366 TKM`\n`5.45x39`\n`5.56x45mm NATO`\n`.300 Blackout`\n`7.62x39mm`\n`7.62x51mm NATO`\n`7.62x54mmR`\n`.338 Lapua Magnum`\n`12.7x55mm STs-130`\n`40x46 mm`\n`Stationary Weapons`", inline: true },
