fcda89f2867f7a5169b2e4c3d3566539be76cefc
Froggi22 <daniel.heinstedt@gmail.com>
2021-12-20 12:41:12 +0100

    2021.12.20.0
    Updated guildCreated

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,14 +1,13 @@
-const { MessageEmbed } = require("discord.js")
-const { embedDesign, general } = require("../config.json")
+/*const { MessageEmbed, Permissions } = require("discord.js")
+const { embedDesign, general } = require("../config.json")*/
 
 module.exports = {
 	async run (guild, client) {
-		const owner = await guild.fetchOwner()
 		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 		/* const date = new Date().toISOString().replace("T", " ").replace("Z", "")
 		console.log(`${date} || Rat God joined a new server "${guild.name}", and is now in "${client.guilds.cache.size}" servers.`) */
 
-		const embed = new MessageEmbed()
+		/*const embed = new MessageEmbed()
 			.setTitle("Thank you for inviting Rat God!")
 			.setDescription(general.descriptionRatGod)
 			.setThumbnail(embedDesign.ratGodImage)
@@ -18,6 +17,7 @@ module.exports = {
 			.setTimestamp()
 
 		const channel = guild.channels.cache.find(channel => channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES))
-		if (channel) channel.send({ embeds: [embed] })
+		console.log(channel.type)
+		if (channel) channel.send({ embeds: [embed] })*/
 	}
 }
