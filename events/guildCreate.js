bef1929dbd8879ac004f6c3cdaffc89e03707e90
Froggi22 <daniel.heinstedt@gmail.com>
2021-12-20 15:40:09 +0100

    2021.12.20.1
    Fixed guildCreate prompt in guild instead of owner

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,13 @@
-/*const { MessageEmbed, Permissions } = require("discord.js")
-const { embedDesign, general } = require("../config.json")*/
+const { MessageEmbed, Permissions } = require("discord.js")
+const { embedDesign, general } = require("../config.json")
 
 module.exports = {
 	async run (guild, client) {
-		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 		/* const date = new Date().toISOString().replace("T", " ").replace("Z", "")
 		console.log(`${date} || Rat God joined a new server "${guild.name}", and is now in "${client.guilds.cache.size}" servers.`) */
+		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 
-		/*const embed = new MessageEmbed()
+		const embed = new MessageEmbed()
 			.setTitle("Thank you for inviting Rat God!")
 			.setDescription(general.descriptionRatGod)
 			.setThumbnail(embedDesign.ratGodImage)
@@ -16,8 +16,7 @@ module.exports = {
 			.setFooter(embedDesign.gameUpdate)
 			.setTimestamp()
 
-		const channel = guild.channels.cache.find(channel => channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES))
-		console.log(channel.type)
-		if (channel) channel.send({ embeds: [embed] })*/
+		const channel = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES))
+		if (channel) channel.send({ embeds: [embed] })
 	}
 }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 module.exports = {
 	async run (guild, client) {
-		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 		/* const date = new Date().toISOString().replace("T", " ").replace("Z", "")
 		console.log(`${date} || Rat God is no longer in "${guild.name}", and is now in "${client.guilds.cache.size}" servers.`) */
+		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
 	}
 }
