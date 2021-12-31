9d10ab5d71af87a6463b921af811b7be8c272a53
Froggi22 <daniel.heinstedt@gmail.com>
2021-12-31 17:14:19 +0100

    2021.12.31.1
    Added key file error explanation.
    Updated game version.

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = {
 	async run (client) {
+		console.log("- - -  Please ignore the message \"[./commands/Keys/keys.js] You forgot run!\" due to it being a work in progress  - - -")
 		const date = new Date().toISOString().replace("T", " ").replace("Z", "")
 		console.log(`${date} || Ready! Logged in as ${client.user.tag}`)
 		client.user.setPresence({ activities: [{ type: "LISTENING", name: `/Help | In ${client.guilds.cache.size} Servers` }] })
