1ae3bb8d200e92424576eb0c01142f9cca47514b
Froggi22 <daniel.heinstedt@gmail.com>
2022-01-12 09:09:05 +0100

    2022.01.12.1
    Updated LatestMessage & permissionMessage & Appreciation letter.

--- commands/General/latest.js
+++ commands/General/latest.js
@@ -10,7 +10,7 @@ module.exports = {
 				.setColor(embedDesign.color)
 				.setAuthor({ name: "Latest update", url: embedDesign.wikiMain, iconURL: embedDesign.ratGodImage })
 				.setDescription(`\`\`\`diff\n# ${general.version}\n\n${general.latestUpdateMessage}\n\`\`\``)
-				.addField("Appreciation letter", `Thank you for inviting and using my bot!\n I appreciate all feedback in the [Support Server](${general.supportInviteLink})!\n`)
+				.addField("Appreciation letter", `Thank you for inviting and using my bot! I'm so thankful for surpassing 100 servers!\n I appreciate all feedback in the [Support Server](${general.supportInviteLink})!\n`)
 				.setFooter({ text: embedDesign.gameUpdate })
 				.setTimestamp()
 			]
