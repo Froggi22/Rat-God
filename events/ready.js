531825713048da7e2e0ee81366da54d3737f8b94
Froggi22 <daniel.heinstedt@gmail.com>
2022-02-11 11:44:09 +0100

    API / JSON Progress

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,8 @@
+// const fetch = require("node-fetch")
+// const fs = require("fs")
+
 module.exports = {
-	run (client) {
+	async run (client) {
 		const startupDate = new Date().toLocaleString().replace(",", "")
 		console.log(`${startupDate} || Ready! Logged in as ${client.user.tag}`)
 		client.user.setPresence({
@@ -9,6 +12,46 @@ module.exports = {
 				name: `/Help | In ${client.guilds.cache.size} Servers`
 			}]
 		})
+
+		// const { fetchedJSON } = await fetch("https://www.reddit.com/r/popular.json")
+		// .then(response => response.json())
+		// const fetch = require("node-fetch")
+
+		/*
+		const url = "https://dev.sp-tarkov.com/SPT-AKI/Server/raw/branch/development/project/assets/database/templates/items.json"
+		const settings = { method: "Get" }
+		const startTime = new Date()
+		fetch(url, settings)
+			.then(res => res.json())
+			.then((json) => {
+				// console.log(json)
+				console.log(`Timedelta > ${new Date() - startTime}ms`)
+				fs.writeFile("tarkovJSON.json", JSON.stringify(json), err => {
+					if (err) console.error(err)
+				})
+			})
+		*/
+
+		/*
+		const startTime2 = new Date()
+		fs.readFile("tarkovJSON.json", "utf8", (err, data) => {
+			if (err) console.log(err)
+			const JSONObj = JSON.parse(data)
+			// const result = JSONObj[Object.keys(JSONObj)[0]]
+			const result = Object.values(JSONObj)
+			console.log(Object.keys(JSONObj).length)
+			let counter = 0
+			for (let item = 0; item < Object.keys(JSONObj).length; item++) {
+				// console.log(`For, Item! ${result[item]._props}`)
+				if (result[item]._props && result[item]._props.Caliber) {
+					console.log(result[item]._props.Caliber.replace("Caliber", ""))
+					counter += 1
+				}
+			}
+			console.log(`Counter > ${counter}!`)
+			console.log(`Timedelta2 > ${new Date() - startTime2}ms`)
+		})
+		*/
 	}
 }
 
