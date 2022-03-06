a4aff1287e4e8a2c580a9b8e3137d5bca72fb25c
Froggi22 <daniel.heinstedt@gmail.com>
2022-03-06 11:58:22 +0100

    Ping command
    Simplified it - using only the heartbeat ping, and consequently integrating it with commandReply.js.
    Removed colorRed.

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -1,19 +1,15 @@
 import { MessageEmbed } from "discord.js"
+import { interactionReply } from "../../commandReply.js"
 import { config } from "../../index.js"
 
-// Would be nice if we could integrate this to commandReply.js ...
-
 export const description = "Pinging responsetimes"
-export async function run (interaction, client) {
-	const embed = new MessageEmbed() // First embed
-		.setColor(config.embedDesign.colorRed)
-		.setAuthor({ name: `🏓 Pong!\nThe heartbeat ping is ${Math.round(client.ws.ping)}ms.` })
-		.setTimestamp()
-	const startTime = new Date()
-	await interaction.reply({ embeds: [embed], fetchReply: true }).catch() // Reply with first embed but wait for an edit
-	embed // Second embed
-		.setColor(config.embedDesign.defaultColor)
-		.setAuthor({ name: `🏓 Pong!\nThe heartbeat ping is ${Math.round(client.ws.ping)}ms.\nThe message round-trip took ${new Date() - startTime}ms.` })
-		.setTimestamp()
-	await interaction.editReply({ embeds: [embed] }).catch() // Edit the message with new info
+export function run (interaction, client) {
+	interactionReply(interaction, {
+		embeds: [new MessageEmbed()
+			.setColor(config.embedDesign.defaultColor)
+			.setTitle("🏓 Pong!")
+			.setDescription(`My ping is **${Math.round(client.ws.ping)}**ms!\nI am online and responsive! 🕙`)
+			.setTimestamp()
+		]
+	})
 }
