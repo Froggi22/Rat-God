23e2ecb199fcf9af23510ea8ba0592189372d65c
Froggi22 <daniel.heinstedt@gmail.com>
2022-03-10 23:47:20 +0100

    node fetch json

--- commands/Ammo/ammo.js
+++ commands/Ammo/ammo.js
@@ -1,6 +1,6 @@
 import { MessageEmbed } from "discord.js"
 import { interactionReply } from "../../commandReply.js"
-import { tarkovJSONAmmo } from "../../events/ready.js"
+import { fetchAmmo } from "../../events/ready.js"
 import { config } from "../../index.js"
 import AsciiTable from "ascii-table"
 
@@ -13,9 +13,11 @@ export const options = [{
 	choices: Object.entries(config.ammo).map(choice => { return { name: choice[0], value: choice[1] } })
 }]
 
+const tarkovJSONAmmo = await fetchAmmo()
+
 export function run (interaction) {
 	const caliber = interaction.options.getString("caliber")
-	const valueToKey = (Object.keys(config.ammo).find(key => config.ammo[key] === caliber))
+	const valueToKey = Object.keys(config.ammo).find(key => config.ammo[key] === caliber)
 
 	const table = new AsciiTable()
 	table.removeBorder()
@@ -30,16 +32,25 @@ export function run (interaction) {
 
 	function pushData (item, itemProps) {
 		console.log(item._name)
+		let itemName = item._name
+			.replace(/_/g, " ")
+			.replace("patron ", "")
+			.split(" ").slice(1).join(" ")
+			.trim()
+			.replace(/\w\S*/g, w => w.match(/[a-z]/) ? w.replace(w.match(/[a-z]/)[0], w.match(/[a-z]/)[0].toUpperCase()) : w)
+		const customSwitcheruu = {
+			"": "B-32 gl",
+			Slug: "Lead Slug",
+			Buckshot: "Buckshot 7"
+		}
+		// if (Object.keys(customSwitcheruu).includes(itemName)) itemName = "B-32 gl"
+		console.log(itemName)
+		const renameThisLaterPls = Object.keys(customSwitcheruu).find(key => customSwitcheruu[key] === itemName)
+		console.log(renameThisLaterPls)
+		if (renameThisLaterPls) itemName = renameThisLaterPls
+		console.log(itemName)
 		tableData.push([
-			item._name
-				.toLowerCase()
-				.replace(/_/g, " ")
-				.replace("patron ", "")
-				.split(" ").slice(1).join(" ") // This instead of something like: .replace(caliber.replace("mm", ""), "")
-				.replace("mm", "")
-				.trim()
-				.replace(/\w\S*/g, w => w.match(/[a-z]/) ? w.replace(w.match(/[a-z]/)[0], w.match(/[a-z]/)[0].toUpperCase()) : console.log(`errorrrrrr > ${w}`)
-				),
+			itemName,
 			itemProps.PenetrationPower,
 			itemProps.ProjectileCount > 1 ? itemProps.Damage * itemProps.ProjectileCount : itemProps.Damage,
 			itemProps.ArmorDamage,
@@ -53,12 +64,8 @@ export function run (interaction) {
 		const item = tarkovJSONAmmo[objNr]
 		const itemProps = item._props
 		if (!itemProps.Caliber) continue
-		if (item._props.Caliber.replace("Caliber", "") === caliber) pushData(item, itemProps)
-		else if (caliber === "127x108" && item._props.Caliber.replace("Caliber", "") === "30x29") pushData(item, itemProps)
-
-		/* TODO
-		FIX > STATIONARY WEAPONS, THERE ARE 2 VALUES NEEDED TO BE SEARCHED FOR 127x108 & 30x29!
-		*/
+		if (itemProps.Caliber.replace("Caliber", "") === caliber) pushData(item, itemProps)
+		else if (caliber === "127x108" && itemProps.Caliber.replace("Caliber", "") === "30x29")	pushData(item, itemProps)
 	}
 	tableData.sort((x, y) => y[1] - x[1] || y[2] - x[2])
 	for (const stat in tableData) {
--- events/ready.js
+++ events/ready.js
@@ -1,41 +1,26 @@
-// const fetch = require("node-fetch")
+// import fetch from "node-fetch"
 import { readFile } from "fs/promises"
 
-const startTime2 = new Date()
-const tarkovJSONRaw = JSON.parse(await readFile("tarkovJSON.json"))
-export const tarkovJSONValues = Object.values(tarkovJSONRaw)
+// Read tarkovJSON.json
 
-export const tarkovJSONAmmo = tarkovJSONValues.filter(Obj => Obj._props && Obj._props.Caliber && Obj._props.ammoType !== "grenade" && Obj._name.toLowerCase() !== "ammo" && !Obj._name.toLowerCase().startsWith("shrapnel"))
+export async function fetchAmmo () {
+	const tarkovJSONRaw = JSON.parse(await readFile("tarkovJSON.json"))
+	/* // const TDurl = "https://raw.githubusercontent.com/TarkovTracker/tarkovdata/master/ammunition.json" // TarkovData JSON
+	const tarkovURL = "https://dev.sp-tarkov.com/SPT-AKI/Server/raw/branch/development/project/assets/database/templates/items.json" // BSG JSON
+	const settings = { method: "GET" }
+	const tarkovJSON = await fetch(tarkovURL, settings)
+		.then(response => response.json())
+		.catch(error => console.log(`Error > ${error}`))
+	return Object.values(tarkovJSON)
+		.filter(Obj => Obj._props && Obj._props.Caliber && Obj._props.ammoType !== "grenade" && Obj._name !== "Ammo" && !Obj._name.startsWith("shrapnel")) */
+	return Object.values(tarkovJSONRaw)
+		.filter(Obj => Obj._props && Obj._props.Caliber && Obj._name !== "Ammo" && !Obj._name.startsWith("shrapnel"))
+}
 
-// Iterate thorugh ammo names
-/* const arr = []
-for (let item = 0; item < tarkovJSONAmmo.length; item++) {
-	if (!arr.includes(tarkovJSONAmmo[item]._props.Caliber)) {
-		arr.push(tarkovJSONAmmo[item]._props.Caliber)
-	}
-} */
-
-/* // Fetching API
-const { fetchedJSON } = await fetch("https://www.reddit.com/r/popular.json")
-.then(response => response.json())
-const fetch = require("node-fetch")
-
-const tarkovURL = "https://dev.sp-tarkov.com/SPT-AKI/Server/raw/branch/development/project/assets/database/templates/items.json" // BSG JSON
-const TDurl = "https://raw.githubusercontent.com/TarkovTracker/tarkovdata/master/ammunition.json" // TarkovData JSON
-const settings = { method: "Get" }
-const startTime = new Date()
-fetch(url, settings)
-.then(res => res.json())
-.then((json) => {
-	console.log(`Timedelta > ${new Date() - startTime}ms`)
-	fs.writeFile("tarkovJSON.json", JSON.stringify(json), err => {
-		if (err) console.error(err)
-	})
-}) */
-
-// Main Ready function
 export async function run (client) {
-	console.log(`Timedelta2 > ${new Date() - startTime2}ms`)
+	const startTime = new Date()
+	// console.log(tarkovJSONAmmo[0]._name)
+	console.log(`Startup delay > ${new Date() - startTime}ms`)
 	const startupDate = new Date().toLocaleString().replace(",", "")
 	console.log(`${startupDate} || Ready! Logged in as ${client.user.tag}`)
 	client.user.setPresence({
@@ -46,18 +31,3 @@ export async function run (client) {
 		}]
 	})
 }
-
-/* Dynamic statistics and information
-	+ BSG has a JSON file with information about all in-game items.
-	~ Fetch the JSON file async on ready event (bot restarts every ~12-24h) and either store it in memory or save it as a JSON file.
-	+ Here we can filter through the items and find whatever we like (ammo).
-	+ When searching for ammo, we will look for a specific caliber.
-
-	+ Then store all the correct calibers and collect their stats (Flesh-, armor dmg).
-	+ Return then that to the user who requested it with the proper formatting.
-
-	~ Ready >
-		~ Fetches API, stores in memory
-	+ Other files e.g Ammo >
-		+ Requires variable & does shit
-*/
