eda8141e7a1889cf10b334e8b23b05f68b99d832
Froggi22 <daniel.heinstedt@gmail.com>
2022-03-16 22:30:48 +0100

    Statistics uptime

--- commands/General/statistics.js
+++ commands/General/statistics.js
@@ -1,6 +1,7 @@
 import { MessageEmbed, version } from "discord.js"
 import { interactionReply } from "../../commandReply.js"
 import { config } from "../../index.js"
+import ms from "ms"
 
 export const description = "Bot statistics"
 export function run (interaction, client) {
@@ -9,9 +10,9 @@ export function run (interaction, client) {
 			.setColor(config.embedDesign.defaultColor)
 			.setThumbnail(config.embedDesign.ratGodImage)
 			.addFields(
-				{ name: "D.JS version", value: `${version}`, inline: true },
+				{ name: "D.JS version", value: `v${version}`, inline: true },
 				{ name: "Node version", value: `${process.version}`, inline: true },
-				{ name: "Uptime", value: `${(client.uptime * 0.001 / 60 / 60).toFixed(2)} hours`, inline: true },
+				{ name: "Uptime", value: (ms(client.uptime)), inline: true },
 				{ name: "Servers", value: `${client.guilds.cache.size}`, inline: true },
 				{ name: "Cached Channels", value: `${client.channels.cache.size}`, inline: true },
 				{ name: "Cached Users", value: `${client.users.cache.size}`, inline: true }
