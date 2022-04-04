92e662d38a843bc5d9a29259d964f6472735b4d5
Froggi22 <daniel.heinstedt@gmail.com>
2022-04-04 11:13:30 +0200

    Moved support to a help button
    Removed support and service status cuz it doesn't work :sadge:

--- commands/General/help.js
+++ commands/General/help.js
@@ -1,18 +1,32 @@
-import { MessageEmbed } from "discord.js"
+import { MessageEmbed, MessageActionRow, MessageButton } from "discord.js"
 import { interactionReply } from "../../utils.js"
 import { config } from "../../index.js"
 
 export const description = "A compact list of Rat Gods commands"
 export function run (interaction) {
-	interactionReply(interaction, {
-		messageEmbed: new MessageEmbed()
-			.setColor(config.embedDesign.color.gold)
-			.setAuthor({ name: "🐀 Rat Gods Commands", url: config.generalLinks.wikiMain })
-			.setDescription(`Leave suggestions to ${config.general.devDiscordTag}.`)
-			.addFields(
-				{ name: "Commands", value: `${config.general.prefixMessage}\n${config.general.helpCommands}\n` },
-				{ name: `Version ${config.general.version}`, value: `${config.general.lastUpdatedMessage}\n${config.general.helpCommandSubtext}` }
-			)
-			.setFooter({ text: config.embedDesign.gameUpdate })
-	})
+	const embed = new MessageEmbed()
+		.setColor(config.embedDesign.color.gold)
+		.setAuthor({ name: "🐀 Rat Gods Commands", url: config.generalLinks.wikiMain })
+		.setDescription(`Leave suggestions to ${config.general.devDiscordTag}.`)
+		.addFields(
+			{ name: "Commands", value: `${config.general.prefixMessage}\n${config.general.helpCommands}\n` },
+			{ name: `Version ${config.general.version}`, value: `${config.general.lastUpdatedMessage}\n${config.general.helpCommandSubtext}` }
+		)
+		.setFooter({ text: config.embedDesign.gameUpdate })
+
+	const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setURL(config.generalLinks.discordInvite)
+				.setLabel("Support")
+				.setStyle("LINK"),
+
+			new MessageButton()
+				.setCustomId("devDiscordTag") // Necessary but useless
+				.setLabel(`Dev: ${config.general.devDiscordTag}`)
+				.setStyle("SECONDARY")
+				.setDisabled(true)
+		)
+
+	return interactionReply(interaction, { messageEmbed: embed, messageComponents: row })
 }
