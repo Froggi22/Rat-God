54cdbf3883c6de8a77e97b9c049e5bfdea25c381
Froggi22 <daniel.heinstedt@gmail.com>
2022-04-07 22:51:46 +0200

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,5 @@
 import { interactionReply, capitalizeWords } from "../utils.js"
 import { config, cooldowns, commands } from "../index.js"
-import ms from "ms"
 
 export function run (interaction, client) {
 	if (interaction.isCommand()) {
@@ -10,7 +9,7 @@ export function run (interaction, client) {
 		// Cooldown
 		const now = Date.now()
 		const expirationTime = cooldowns.get(interaction.user.id) + config.general.cooldown
-		if (now < expirationTime) return interactionReply(interaction, { messageContent: `Please wait **${ms(expirationTime - now)}** before reusing the \`${capitalizeWords(command.name)}\` command.` })
+		if (now < expirationTime) return interactionReply(interaction, { messageContent: `Please wait **${((expirationTime - now) * 0.001).toFixed()}s** before reusing the \`${capitalizeWords(command.name)}\` command.` })
 
 		cooldowns.set(interaction.user.id, now) // Set user cooldown
 		setTimeout(() => cooldowns.delete(interaction.user.id), config.general.cooldown)
