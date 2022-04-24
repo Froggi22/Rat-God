45dfe284be1c01e7e5b7908eb96259f7587ca3a0
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-04-24 17:01:16 +0700

    another attempt to fix missing permissions

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -10,14 +10,19 @@ export function run (guild, client) {
 		}]
 	})
 
-	const embed = new MessageEmbed()
-		.setColor(config.embedDesign.color.gold)
-		.setThumbnail(config.generalLinks.botProfileImage)
-		.setTitle("Thank you for inviting Rat God!")
-		.setDescription("Rat God will help you in Escape From Tarkov, by accessing in-game information and delivering it to you!")
-		.addField(`${config.general.prefixMessage}`, `To understand why Rat God requires certain permissions, please view the "Permissions" section on [our Github page](${config.generalLinks.github}).\n`)
-		.setFooter({ text: config.embedDesign.gameUpdate })
+	if (!guild.available) return // if guild is not available, return
 
-	const channel = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.viewable && channel.isText() && channel.permissionsFor(guild.me).has(Permissions.FLAGS.VIEW_CHANNEL) && channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES)) // Find the first textchannel where the bot can send the welcome message with specific conditions
-	if (channel) channel.send({ embeds: [embed] }).catch() // If such channel exists, send the embed
+	const channel = guild.channels.cache.find(channel => channel.isText() && channel.type === "GUILD_TEXT" && channel.viewable && channel.permissionsFor(guild.me).has(Permissions.FLAGS.VIEW_CHANNEL) && channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES)) // Find the first viewable, text channel where the bot can view the channel and send the welcome message
+
+	if (channel) { // If such channel exists, send the embed
+		channel.send({
+			embeds: [new MessageEmbed()
+				.setColor(config.embedDesign.color.gold)
+				.setThumbnail(config.generalLinks.botProfileImage)
+				.setTitle("Thank you for inviting Rat God!")
+				.setDescription("Rat God will help you in Escape From Tarkov, by accessing in-game information and delivering it to you!")
+				.addField(`${config.general.prefixMessage}`, `To understand why Rat God requires certain permissions, please view the "Permissions" section on [our Github page](${config.generalLinks.github}).\n`)
+				.setFooter({ text: config.embedDesign.gameUpdate })]
+		}).catch(console.log)
+	}
 }
